7d43c097e0f8b16d732668bd7c512a4e920c9063
Alegnta <alegnta88@gmail.com>
2024-03-02 12:13:43 +0300

    my-laravel-project

---
+++ resources/views/home.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-3 p-3">
+            <img src="images/unnamed.jpg" style="width: 100px; height: 100px;"  class="rounded-circle">
+        </div>
+        <div class="col-9">
+            <div><h1>Alegnta Lolamo</h1></div>
+        </div>
+    </div>
+</div>
+@endsection
